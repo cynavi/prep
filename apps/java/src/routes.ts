ad03d421c9172fd518230bb54a50aaa6808633d8
cynavi <basnetavinay@gmail.com>
2024-07-09 13:28:55 +0545

    Rearrange routes.

--- apps/java/src/routes.ts
+++ apps/java/src/routes.ts
@@ -8,14 +8,16 @@ export const stringRoutes = {
     {
       label: 'Reference vs value comparison (== vs equals())',
       link: '/core/string/reference-vs-value-comparison'
-    }]
+    }],
+  collapsed: true
 } as const;
 
 export const oopRoutes = {
   label: 'OOP',
   items: [
     { label: 'Multiple inheritance', link: '/core/oop/multiple-inheritance' }
-  ]
+  ],
+  collapsed: true
 } as const;
 
 export const collectionsRoutes = {
@@ -23,14 +25,24 @@ export const collectionsRoutes = {
   items: [
     { label: 'HashSet internal working', link: '/core/collections/hashset-internal-working'},
     { label: 'Fail-fast vs fail-safe', link: '/core/collections/fail-fast-vs-fail-safe'}
-  ]
+  ],
+  collapsed: true
 } as const;
 
 export const threadRoutes = {
   label: 'Thread',
   items: [
     { label: 'Creating thread', link: '/core/thread/creating-thread'}
-  ]
+  ],
+  collapsed: true
+} as const;
+
+export const securityRoutes = {
+  label: 'Security',
+  items: [
+    { label: 'Java Sandbox', link: '/core/java-sandbox' }
+  ],
+  collapsed: true
 } as const;
 
 export const jdbcRoutes = {
@@ -38,6 +50,7 @@ export const jdbcRoutes = {
   items: [
     { label: 'execute() vs executeQuery() vs executeUpdate()', link: '/jdbc/execute-vs-execute-query-vs-execute-update'},
     { label: 'Statement vs PreparedStatement vs CallableStatement', link: '/jdbc/statement-vs-prepared-statement-vs-callable-statement'}
-  ]
+  ],
+  collapsed: true
 } as const;
 
